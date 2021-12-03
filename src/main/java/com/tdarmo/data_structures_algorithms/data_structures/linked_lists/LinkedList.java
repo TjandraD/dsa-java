8dd5c9ca8a3b8cb85a8bfb7e54da969b6e7c9868
TjandraD <tjandradarmo@protonmail.com>
2021-12-03 11:55:50 +0700

    feat: Reverse Singly Linked List with reverse() method

---
+++ src/main/java/com/tdarmo/data_structures_algorithms/data_structures/linked_lists/LinkedList.java
@@ -0,0 +1,112 @@
+package com.tdarmo.data_structures_algorithms.data_structures.linked_lists;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LinkedList {
+    private Node head;
+    private Node tail;
+    private int length;
+
+    LinkedList(int value) {
+        this.head = new Node(value);
+        this.tail = this.head;
+        this.length = 1;
+    }
+
+    @Override
+    public String toString() {
+        return "LinkedList { \nhead: " + head + ", \ntail: " + tail + ", \nlength: " + length + " }";
+    }
+
+    public void append(int value) {
+        Node newNode = new Node(value);
+        tail.setNext(newNode);
+        tail = newNode;
+        length++;
+    }
+
+    public void prepend(int value) {
+        Node newNode = new Node(value);
+        newNode.setNext(head);
+        head = newNode;
+        length++;
+    }
+
+    public void insert(int index, int value) {
+        if (index == 0) {
+            prepend(value);
+            return;
+        } else if (index >= length) {
+            append(value);
+            return;
+        }
+
+        Node previousNode = traverseToIndex(index - 1);
+        Node newNode = new Node(value);
+        Node nextNode = previousNode.getNext();
+        previousNode.setNext(newNode);
+        newNode.setNext(nextNode);
+        length++;
+    }
+
+    public Node traverseToIndex(int index) {
+        int nodeIndex = 0;
+        Node currentNode = head;
+        while (index != nodeIndex) {
+            currentNode = currentNode.getNext();
+            nodeIndex++;
+        }
+
+        return currentNode;
+    }
+
+    public void remove(int index) {
+        length--;
+        if (index == 0) {
+            head = head.getNext();
+            return;
+        } else if (index >= length) {
+            Node newTail = traverseToIndex(length - 2);
+            newTail.setNext(null);
+            tail = newTail;
+            return;
+        }
+
+        Node previousNode = traverseToIndex(index - 1);
+        Node currentNode = previousNode.getNext();
+        previousNode.setNext(currentNode.getNext());
+    }
+
+    public void reverse() {
+        if (head.getNext() == null) {
+            return;
+        }
+
+        Node node = head;
+        Node previousNode = null;
+        Node nextNode;
+
+        while (node != null) {
+            nextNode = node.getNext();
+            node.setNext(previousNode);
+            previousNode = node;
+            node = nextNode;
+        }
+
+        Node head = this.head;
+        this.head = this.tail;
+        this.tail = head;
+    }
+
+    public List<Integer> printList() {
+        List<Integer> array = new ArrayList<>();
+        Node currentNode = head;
+        while (currentNode != null) {
+            array.add(currentNode.getValue());
+            currentNode = currentNode.getNext();
+        }
+
+        return array;
+    }
+}
--- src/main/java/com/tdarmo/data_structures_algorithms/data_structures/linked_lists/Main.java
+++ src/main/java/com/tdarmo/data_structures_algorithms/data_structures/linked_lists/Main.java
@@ -2,6 +2,23 @@ package com.tdarmo.data_structures_algorithms.data_structures.linked_lists;
 
 public class Main {
     public static void main(String[] args) {
+        singlyLinkedList();
+        doublyLinkedList();
+    }
+
+    public static void singlyLinkedList() {
+        LinkedList myLinkedList = new LinkedList(1);
+        myLinkedList.append(2);
+        myLinkedList.append(3);
+        myLinkedList.append(4);
+        myLinkedList.remove(1);
+        myLinkedList.insert(4, 5);
+        myLinkedList.insert(5, 5);
+        myLinkedList.reverse();
+        System.out.println(myLinkedList.printList());
+    }
+
+    public static void doublyLinkedList() {
         DoublyLinkedList myLinkedList = new DoublyLinkedList(10);
         myLinkedList.append(5);
         myLinkedList.append(16);
