001dab8ca00c847c9594e7814ab80250018c315e
TjandraD <tjandradarmo@protonmail.com>
2021-12-14 10:22:08 +0700

    feat: Create bubbleSort within Sort class

---
+++ src/main/java/com/tdarmo/data_structures_algorithms/algorithms/sorting/Main.java
@@ -0,0 +1,13 @@
+package com.tdarmo.data_structures_algorithms.algorithms.sorting;
+
+import java.util.Arrays;
+
+import static com.tdarmo.data_structures_algorithms.algorithms.sorting.Sort.*;
+
+public class Main {
+    public static void main(String[] args) {
+        final int[] number = new int[]{99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0};
+        final int[] sortedNumber = bubbleSort(number);
+        System.out.println(Arrays.toString(sortedNumber));
+    }
+}
---
+++ src/main/java/com/tdarmo/data_structures_algorithms/algorithms/sorting/Sort.java
@@ -0,0 +1,19 @@
+package com.tdarmo.data_structures_algorithms.algorithms.sorting;
+
+public class Sort {
+    static int[] bubbleSort(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                if (j == array.length - 1) {
+                    break;
+                } else if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+
+        return array;
+    }
+}
