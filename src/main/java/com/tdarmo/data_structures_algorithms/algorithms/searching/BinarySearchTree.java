b958722d27037bfcd998cd8a93cf290afdb1f193
TjandraD <tjandradarmo@protonmail.com>
2021-12-16 17:27:03 +0700

    feat: Create breadthFirstSearch within BST class

---
+++ src/main/java/com/tdarmo/data_structures_algorithms/algorithms/searching/BinarySearchTree.java
@@ -0,0 +1,127 @@
+package com.tdarmo.data_structures_algorithms.algorithms.searching;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class BinarySearchTree {
+    private Node root;
+
+    public BinarySearchTree() {
+        this.root = null;
+    }
+
+    public void insert(int value) {
+        Node newNode = new Node(value);
+        if (this.root == null) {
+            this.root = newNode;
+            return;
+        }
+
+        Node currentNode = this.root;
+        while (true) {
+            if (value < currentNode.getValue()) {
+                if (currentNode.getLeft() == null) {
+                    currentNode.setLeft(newNode);
+                    break;
+                }
+                currentNode = currentNode.getLeft();
+            } else {
+                if (currentNode.getRight() == null) {
+                    currentNode.setRight(newNode);
+                    break;
+                }
+                currentNode = currentNode.getRight();
+            }
+        }
+    }
+
+    public Node lookup(int value) {
+        Node currentNode = this.root;
+
+        while (currentNode != null) {
+            if (value < currentNode.getValue()) {
+                currentNode = currentNode.getLeft();
+            } else if (value > currentNode.getValue()) {
+                currentNode = currentNode.getRight();
+            } else {
+                return currentNode;
+            }
+        }
+
+        return null;
+    }
+
+    public void remove(int value) {
+        Node parentNode = this.root;
+
+        while (true) {
+            if (value < parentNode.getValue()) {
+                if (parentNode.getLeft().getValue() == value) {
+                    Node delete = parentNode.getLeft();
+                    if (delete.getRight() == null) {
+                        parentNode.setLeft(delete.getLeft());
+                    } else if (delete.getRight().getRight() != null) {
+                        Node rightChild = delete.getRight().getRight();
+                        rightChild.setRight(delete.getRight());
+                        rightChild.setLeft(delete.getLeft());
+                        parentNode.setLeft(rightChild);
+                    } else {
+                        Node replace = delete.getRight();
+                        replace.setLeft(delete.getLeft());
+                        parentNode.setLeft(replace);
+                    }
+
+                    break;
+                } else {
+                    parentNode = parentNode.getLeft();
+                }
+            } else {
+                if (parentNode.getRight().getValue() == value) {
+                    Node delete = parentNode.getRight();
+                    if (delete.getRight() == null) {
+                        parentNode.setRight(delete.getRight());
+                    } else if (delete.getRight().getRight() != null) {
+                        Node rightChild = delete.getRight().getRight();
+                        rightChild.setRight(delete.getRight());
+                        rightChild.setLeft(delete.getLeft());
+                        parentNode.setLeft(rightChild);
+                    } else {
+                        Node replace = delete.getRight();
+                        replace.setLeft(delete.getLeft());
+                        parentNode.setLeft(replace);
+                    }
+
+                    break;
+                } else {
+                    parentNode = parentNode.getRight();
+                }
+            }
+        }
+    }
+
+    public List<Integer> breadthFirstSearch() {
+        List<Integer> list = new ArrayList<>();
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(this.root);
+
+        while (queue.size() > 0) {
+            Node currentNode = queue.poll();
+            list.add(currentNode.getValue());
+            if (currentNode.getLeft() != null) {
+                queue.add(currentNode.getLeft());
+            }
+            if (currentNode.getRight() != null) {
+                queue.add(currentNode.getRight());
+            }
+        }
+
+        return list;
+    }
+
+    @Override
+    public String toString() {
+        return "BinarySearchTree {root: " + this.root + "}";
+    }
+}
---
+++ src/main/java/com/tdarmo/data_structures_algorithms/algorithms/searching/Main.java
@@ -0,0 +1,15 @@
+package com.tdarmo.data_structures_algorithms.algorithms.searching;
+
+public class Main {
+    public static void main(String[] args) {
+        BinarySearchTree binarySearchTree = new BinarySearchTree();
+        binarySearchTree.insert(9);
+        binarySearchTree.insert(4);
+        binarySearchTree.insert(6);
+        binarySearchTree.insert(20);
+        binarySearchTree.insert(170);
+        binarySearchTree.insert(15);
+        binarySearchTree.insert(1);
+        System.out.println(binarySearchTree.breadthFirstSearch());
+    }
+}
